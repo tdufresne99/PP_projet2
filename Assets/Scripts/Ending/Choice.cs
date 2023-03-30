acc1b5e348a0c6d9d604781d0d8ea0e4c0a79587
Justine Albiges <2051530@cstj.qc.ca>
2023-03-30 15:30:01 -0400

    Ajout des endings

---
+++ Assets/Scripts/Ending/Choice.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace Ending
+{
+    public class Choice : MonoBehaviour
+    {
+        [SerializeField] private MessagesHolder _messagesHolderCS;
+        private Animator _anim;
+        void Awake()
+        {
+            _anim = GetComponent<Animator>();
+        }
+
+        public void OnChoiceClick()
+        {
+            if (_messagesHolderCS.clickable) 
+            {
+                if(_messagesHolderCS.Index == 0) _messagesHolderCS.ShowAnswer();
+                else _messagesHolderCS.StartAnswerSlide();
+
+                Debug.Log("supposed to popOut");
+                _anim.SetTrigger("popOut");
+                _anim.SetTrigger("popOut");
+            }
+        }
+    }
+}
